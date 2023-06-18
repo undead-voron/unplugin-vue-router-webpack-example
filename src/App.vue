d66495f803009f26cc012d962387a5b707cc17d6
Den <denis.inozemtsev@btb.dev>
2023-06-18 16:00:39 +0400

    add simple repo

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { useRoute } from 'vue-router/auto'
+
+const route = useRoute()
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <mark>current page name is: {{ route.name }}</mark>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <router-view></router-view>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { definePage } from 'vue-router/auto'
+
+definePage({
+  meta: {
+    index: 1,
+  }
+})
+</script>
+
+<template>
+  <main>Home</main>
+</template>
